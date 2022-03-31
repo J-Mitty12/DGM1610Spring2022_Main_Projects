89d95fb337aff462052de7cd86a27803e2b56d51
Julian Mitton <jdmitton12@gmail.com>
2022-03-30 22:08:53 -0600

    Added Challenge 3

---
+++ Challenge 3/Assets/Challenge 3/Scripts/SpawnManagerX.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] objectPrefabs;
+    private float maxSpawnHeight = 15f;
+    private float minSpawnHeight = 5f;
+    private float maxRepeatRate = 4f;
+    private float minRepeatRate = .75f;
+    private float timer;
+    private float repeatRate;
+    private float spawnHeight;
+
+    private PlayerControllerX playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        repeatRate = Random.Range(minRepeatRate, maxRepeatRate);
+        spawnHeight = Random.Range(minSpawnHeight, maxSpawnHeight);
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > repeatRate)
+        {
+            SpawnObjects();
+            repeatRate = Random.Range(minRepeatRate, maxRepeatRate);
+            spawnHeight = Random.Range(minSpawnHeight, maxSpawnHeight);
+            timer = 0;
+        }
+    }
+
+    // Spawn obstacles
+    void SpawnObjects ()
+    {
+        // If game is still active, spawn new object
+        if (!playerControllerScript.gameOver == false)
+        {
+            Vector3 spawnLocation = new Vector3(30, spawnHeight, 0);
+            int index = Random.Range(0, objectPrefabs.Length);
+            Instantiate(objectPrefabs[index], spawnLocation, objectPrefabs[index].transform.rotation);
+        }
+    }
+}
